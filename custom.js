18ea759b450053ac447c4895e1690606e439f536
SlavCO <slavick.slavick@gmail.com>
2018-02-03 19:51:38 +0000

    added elements, re-styled for mobile first approach

---
+++ custom.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('.button');
+const text = document.querySelector('.about-section__content__text');
+var count = 0;
+button.addEventListener('click', function(e) {
+    e.preventDefault();
+    if(count >= 1){
+        count = 0;
+        text.style.display = '-webkit-box';
+        button.innerHTML = 'Read More';
+        return;
+    } else {
+        text.style.display = 'block';
+        button.innerHTML = 'Read Less';
+    }
+    count++;
+});
